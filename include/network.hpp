4938c7257d8a2283327fd0888d1f51a143454113
Kyle Davison <kyle.davison@vertigofarms.com>
2020-12-30 08:56:59 -0700

    update!

---
+++ include/network.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <steamworks/iSteamNetworkingSockets.h>
+#include "utility/copyable.hpp"
+#include <cstdint>
+
+namespace bulldog
+{
+namespace network
+{
+class Server
+  : public ::bulldog::utility::copyable<false>
+{
+public:
+
+private:
+
+};
+
+class Client
+  : public ::bulldog::utility::copyable<false>
+{
+public:
+
+private:
+
+}
+
+}
+}
---
+++ source/server.cc
@@ -0,0 +1,21 @@
+#include <steamworks/steam_api.h>
+#include <cstdint>
+#include <cstdlib>
+
+#include <iostream>
+
+int32_t main(int argv, char* argc[]) {
+  if ( SteamAPI_RestartAppIfNecessary( k_uAppIdInvalid )) {
+    return EXIT_FAILURE;
+  }
+
+  if ( !SteamAPI_Init() ) {
+    return EXIT_FAILURE;
+  }
+
+  
+
+
+  SteamAPI_Shutdown();
+  return 0;
+}
